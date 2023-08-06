6a33fd24e636b07f773179d269367b11d0da3708
Michael Shu <michael.shu@thermovisionusa.com>
2023-08-06 13:43:20 -0700

    Route /images/[width]x[height]

---
+++ src/routes/images/[width]x[height]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	// import type { PageData } from './$types';
+	import { page } from '$app/stores';
+
+	let { width, height } = $page.params;
+	// export let data: PageData;
+</script>
+
+<h1>Imagze size: width:{width} height: {height}</h1>
+
+<pre>
+        {JSON.stringify($page, null, 2)}
+</pre>
--- src/routes/posts/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageLoad } from './$types';
-import { error } from '@sveltejs/kit';
-
-export const load = (async () => {
-	// return {};
-	// throw new Error('Yikes');
-	throw error(404, { message: 'Yikes' });
-}) satisfies PageLoad;
