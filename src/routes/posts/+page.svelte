760d0e6eb44345e16b48a5e9104beeb52e40aac8
Michael Shu <michael.shu@thermovisionusa.com>
2023-08-09 00:01:22 -0700

    SVEL-5 Destructure posts from data. Put it behind $: to make it reactive.

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -2,13 +2,15 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	$: ({ posts } = data);
 </script>
 
 <h1>Posts</h1>
 
-<p>Posts length: {data.posts.length}</p>
+<p>Posts length: {posts.length}</p>
 
-{#each data.posts as { slug, title }}
+{#each posts as { slug, title }}
 	<ul>
 		<a href="/posts/{slug}">{title}</a>
 	</ul>
